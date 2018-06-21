e55262748c304f4b9e9b532f3f775c3caa96cb78
Simon <313460010@qq.com>
2018-06-22 00:03:29 +0800

    Add my-todolist

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <input type="text" name="" v-model="newText">
+    <button @click="addText()">添加</button>
+    <br>
+    <hr>
+    <br>
+    <ul>
+      <li v-for="(item, index) in list">
+        {{item.text}}
+        <button @click="deleteText(index)">删除</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      list:[],
+      newText:"",
+    }
+  },
+  methods:{
+    addText() {
+      if (this.newText.trim() != "") {
+        var newItem = {'text':this.newText};
+        this.list.push(newItem);
+        this.newText = "";
+      }
+    },
+    deleteText(index) {
+      this.list.splice(index,1);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
